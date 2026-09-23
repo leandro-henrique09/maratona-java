5dcb06cf2403f42ca042d176fbd938f589944334
leandro-henrique09 <leandrohdc09@gmail.com>
2026-09-23 17:11:29 -0300

    Estudo sobre POO e inicio com Objetos.

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -19,4 +19,29 @@ public class Calculadora {
         }
         return num1 / num2;
     }
+
+    public double divideDoisNumero02(double num1, double num2) {
+        if(num2 != 0){
+            return num1 / num2;
+        } else {
+//            System.out.println("Não existe divisão por zero.");
+            return 0;
+        }
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if(num2 == 0){
+            System.out.println("Não existe divisão por zero");
+        } else {
+            System.out.println(num1/num2);
+        }
+    }
+
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("num1"+ num1);
+        System.out.println("num2"+ num2);
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -7,5 +7,8 @@ public class CalculadoraTest03 {
         Calculadora calculadora = new Calculadora();
         double result = calculadora.divideDoisNumero(20,2);
         System.out.println(result);
+        System.out.println(calculadora.divideDoisNumero02(20,0));
+        System.out.println("--------------");
+        calculadora.imprimeDivisaoDeDoisNumeros(86,0);
     }
 }
